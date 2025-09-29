c84856ea2cf43016118122d67c97dd26cee66dd2
matiaszzz <165030231+matiaszzz@users.noreply.github.com>
2025-09-29 15:12:18 -0300

    Se agrego el contador usando useEffect al juego de colores Aramayo Matias

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
 import Saludo from './assets/components/Saludo.jsx'
+import JuegoColores from './assets/components/JuegoColores.jsx';
+import {Container, Row, Col} from 'react-bootstrap'
+
 
 function App() {
 
   //variables
-  let nombre = 'nombre';
+  let nombre = 'EL GRUPO 9';
   let apellido = 'apellido';
   let edad = 20;
 
   return (
     <>
-     <Saludo nA ={nombre} aA={apellido} edad={edad}></Saludo>
+    <Container>
+      <Row>
+      <Col><Saludo nA ={nombre} aA={apellido} edad={edad}></Saludo></Col>
+      </Row>
+      <Row>
+      <Col><JuegoColores /></Col>
+      </Row>
+    </Container>
+     
+     
     </>
   )
 }
--- src/assets/components/JuegoColores.jsx
+++ src/assets/components/JuegoColores.jsx
@@ -1,32 +1,46 @@
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
 
-function JuegoColores(){
+function JuegoColores() {
     const colores = ['#007bff', '#dc3545', '#29a783', '#1fd71fff', '#81bd', '#6c757d'];
-    const [colorDeBotones, setColorDeBotones] = useState(colores.slice(0, 5));
-    const [segundos, setSegundos] = useState (0);
+    const [colorDeBotones, setColorDeBotones] = useState(colores.slice(0, 6));
+    const [mensaje, setMensaje] = useState('');
+    const [contador, setContador] = useState(0);
+
+
+    //useEffect
+    useEffect(() => {
+        console.log(contador);
+        setMensaje(`Intentos ${contador}`);
+        
+    }, [contador]);
 
     const manejarClick = () => {
         const coloresNuevos = colorDeBotones.map(() => {
             const indiceAleatorio = Math.floor(Math.random() * colores.length);
+            return colores[indiceAleatorio]
+            
         });
         setColorDeBotones(coloresNuevos);
+        setContador(contador+1);
     };
 
-    return(
+    return (
+        <div>
+            <h1>Juego de Colores</h1>
+            <p>Presiona un boton </p>
             <div>
-                <h1>Juego de Colores</h1>
-                <p>Presiona un boton </p>
-                <div>
-                    {colorDeBotones.map((c, i) => (
-                        <button
-                            key={i}
-                            onClick={manejarClickEnElBoton}
-                            style={{ backgroundColor: c }}
-                        >
-                            Boton {i + 1}
-                        </button>
-                    ))}
-                </div>
+                {colorDeBotones.map((c, i) => (
+                    <button
+                        key={i}
+                        onClick={manejarClick}
+                        style={{ backgroundColor: c }}
+                    >
+                        Boton {i + 1}
+                    </button>
+                ))}
             </div>
+            <h2>{mensaje}</h2>
+        </div>
     )
 }
+export default JuegoColores;
